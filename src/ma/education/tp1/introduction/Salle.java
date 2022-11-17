40f4a6fa75ff9ea0b282a7b98963d0192b11d81a
mohamed amine <amine.mohamed.rg@gmail.com>
2022-11-17 12:34:16 +0100

    tp1, make some comment on the code

--- src/ma/education/tp1/introduction/Salle.java
+++ src/ma/education/tp1/introduction/Salle.java
@@ -1,17 +1,21 @@
 package ma.education.tp1.introduction;
 
 public class Salle {
+    /*Class Salle attributes*/
       long id;
       String nom;
 
+      /*Class Salle default constructor*/
     public Salle() {
 
     }
 
+    /*Class Salle constructor with 1 parameter*/
     public Salle(String nom) {
         this.nom = nom;
     }
 
+    /*Class Salle constructor with 2 parameter*/
     public Salle(long id, String nom) {
         this.id = id;
         this.nom = nom;
--- src/ma/education/tp1/introduction/Test.java
+++ src/ma/education/tp1/introduction/Test.java
@@ -5,15 +5,15 @@ public class Test {
 
 
     public static void main(String[] args) {
-        Salle o1;
-        Salle o2;
-        Salle o3;
 
-//        o1 = new Salle();
-        o2 = new Salle("Salle Informatique");
-        o3 = new Salle(2, "Salle des cours");
-//        System.out.println("o1 -> id = " + o1.id);
-//        System.out.println("o1 -> nom = " + o1.nom);
+        /*Creation of 3 objects: o1, o2, o3*/
+        Salle o1 = new Salle();
+        Salle  o2 = new Salle("Salle Informatique");
+        Salle o3 = new Salle(2, "Salle des cours");
+
+        /*Show the value of objects : o1, o2, o3 */
+        System.out.println("o1 -> id = " + o1.id);
+        System.out.println("o1 -> nom = " + o1.nom);
         System.out.println("o2 -> id = " + o2.id);
         System.out.println("o2 -> nom = " + o2.nom);
         System.out.println("o3 -> id = " + o3.id);
